7f352cc272484bc55141ef9e6ceb8ac4174fed94
sxqnicholas96 <sxq.nicholas96@gmail.com>
2019-02-23 17:17:16 +0800

    Fixed redux shit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { configureStore } from "../../store";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 export default class Home extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
+      <Provider>
+        <View>
+          <Text> textInComponent </Text>
+        </View>
+      </Provider>
+    );
   }
 }
