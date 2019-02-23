5f9a2311fcbd741e60d5debd7e357f4e9d5cfb78
sxqnicholas96 <sxq.nicholas96@gmail.com>
2019-02-23 14:42:20 +0800

    project init

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import App from "./src/App";
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
 
-App();
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
