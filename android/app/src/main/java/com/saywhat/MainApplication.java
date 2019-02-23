377e6b1d9504de4c32e40ede3948a5b83689f53c
sxqnicholas96 <sxq.nicholas96@gmail.com>
2019-02-23 22:31:29 +0800

    fullscreen portrait video

--- android/app/src/main/java/com/saywhat/MainApplication.java
+++ android/app/src/main/java/com/saywhat/MainApplication.java
@@ -3,6 +3,7 @@ package com.saywhat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
             new ReactVideoPackage(),
             new RNGestureHandlerPackage()
       );
