7b3c872d0b281905824d802b0035716b64bc9bf1
jeromeoooo <jeromeoo92@gmail.com>
2019-02-23 23:24:17 +0800

    added button and textinput

--- src/components/New/TextEdit/index.js
+++ src/components/New/TextEdit/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Button } from "react-native-elements";
 
 export default class index extends Component {
   render() {
     return (
       <View>
+        <TextInput />
+        <Button title="Cancel" />
+        <Button title="Publish" />
         <Text> TextEdit </Text>
       </View>
     );
