d422d7e11f5abb66e57a51bd115fd0458ea8c00a
jonathanchow <jonathanchowjh@gmail.com>
2019-02-24 00:05:55 +0800

    redux seed data

--- src/components/Gallery/Video/index.js
+++ src/components/Gallery/Video/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 
-export default class index extends Component {
+export default class Video extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Text> Video </Text>
       </View>
     )
   }
